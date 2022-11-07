98a51a1a97434a2e760096a595bcee462a338748
Fast <Fast@DESKTOP-A1OJEAO>
2022-11-06 22:25:03 -0700

    get and getAll changed

--- src/java/dataaccess/UserDB.java
+++ src/java/dataaccess/UserDB.java
@@ -1,14 +1,88 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package dataaccess;
 
-/**
- *
- * @author Fast
- */
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import models.Role;
+import models.User;
+
 public class UserDB {
+    public List<User> getAll(String owner) throws Exception {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        
+        try {
+            //get list of all users
+            List<User> users = em.createNamedQuery("User.findAll", User.class).getResultList();
+            return users;
+        } finally {
+            em.close();
+        }
+    }
     
+     public User get(String email) throws Exception {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        
+        try {
+            User user = em.find(User.class, email);
+            return user;
+        } finally { 
+            em.close();
+        }
+    }
+    
+    public void insert(User user) throws Exception {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+
+        try {
+
+            Role role = user.getRole();
+            role.getUserList().add(user);
+            trans.begin();
+            em.persist(user);
+            em.merge(role);
+            trans.commit();
+        } catch (Exception e) {
+            trans.rollback();
+        } finally {
+           em.close();
+        }
+    }
+    
+     public void update(User user) throws Exception {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+
+        try {
+            trans.begin();
+            em.merge(user);
+            trans.commit();
+        } catch (Exception e) {
+            trans.rollback();
+        } finally {
+           em.close();
+        }
+    }
+
+      public void delete(User user) throws Exception {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+
+        try {
+
+            Role role = user.getRole();
+            role.getUserList().remove(user);
+            trans.begin();
+            em.remove(em.merge(user));
+            em.merge(role);
+            trans.commit();
+        } catch (Exception e) {
+            trans.rollback();
+        } finally {
+           em.close();
+        }
+    }
 }
+
