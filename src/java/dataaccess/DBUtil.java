f393e3614a4f8f8e9d4107212dc63572ac8605e0
Fast <Fast@DESKTOP-A1OJEAO>
2022-11-06 21:50:34 -0700

    connection pool deleted and dbutil updated

--- src/java/dataaccess/DBUtil.java
+++ src/java/dataaccess/DBUtil.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dataaccess;
 
-/**
- *
- * @author Fast
- */
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class DBUtil {
-    
+    private static final EntityManagerFactory emf =
+        Persistence.createEntityManagerFactory("UsersPU");
+
+    public static EntityManagerFactory getEmFactory() {
+        return emf;
+    }
 }
